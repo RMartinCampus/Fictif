820b58965cc3d468276558c865963525a8896943
R.MARTIN <remy.martin@le-campus-numerique.fr>
2024-01-04 16:00:26 +0100

    feat:Ajout d'un message Ajoute un messsage dans la console

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    static void main(String[ ] args){
+        System.out.println("Bonjour !");
+        System.out.println("Bientot une nouvelle feature va arriver!");
+    }
+}
