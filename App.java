f3160521ab516ebb950a5bc71b4a1b0aab6b6f07
R.MARTIN <remy.martin@le-campus-numerique.fr>
2024-01-04 16:14:30 +0100

    refactor:Modifie la string affiché

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     static void main(String[ ] args){
         System.out.println("Bonjour !");
-        System.out.println("Bientot une nouvelle feature va arriver!");
+        System.out.println("De nouvelles feature arrive fort!");
     }
 }
